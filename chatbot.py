aeb8027623522076425ccf6a652730dae78b83be
Caio Hoeller <71030408+CaioHoeller@users.noreply.github.com>
2020-10-22 11:46:28 -0300

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,16 @@
+print('====Seja bem vindo(a) ao Chattercat!====')
+print('')
+print('Olá, meu nome é Chattercat!')
+print('') 
+name = input('Qual é o seu nome? ')
+print('')
+print('{}? Nome bonito!' .format(name))
+print('')
+nick = input('Como você quer que eu te chame? ')
+print('')
+print('Ok! Irei te chamar de {}.' .format(nick))
+
+
+
+
+    
